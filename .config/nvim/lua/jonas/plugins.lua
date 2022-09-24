b83bbd5549117d68395f3a091f9bf5d50c0d5afb
Jonas Osmann <info@jonasosmann.com>
2022-09-24 01:23:31 -0400

    [feat] install Telescope fuzzy finder plugin
    Install working version of Telescope fuzzy finder plugin with custom
    keymaps.

--- .config/nvim/lua/jonas/plugins.lua
+++ .config/nvim/lua/jonas/plugins.lua
@@ -74,6 +74,9 @@ return packer.startup(function(use)
   use "williamboman/mason.nvim" -- Language server installer. Replaces nvim-lsp-installer.
   use "williamboman/mason-lspconfig.nvim" -- Used to close some gaps between mason.nvim and lspconfig
 
+  -- Telescope
+  use "nvim-telescope/telescope.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
