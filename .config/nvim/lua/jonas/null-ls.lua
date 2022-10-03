f93bb79cfdb010ff7dedf4b26afa8183ae7129d1
jono3030 <j.osmann@mail.utoronto.ca>
2022-10-02 23:07:43 -0400

    [feat] change Lua linter from luacheck to selene
    Changed linter for Lua from luacheck to selene. Removed Lua and Luarocks
    from Dockerfile as it is not required for selene. Need to find a fix for
    duplicated line in options that sets backup directory. Automatic
    installation of linters and formatters is not working yet through
    mason-config.

--- .config/nvim/lua/jonas/null-ls.lua
+++ .config/nvim/lua/jonas/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-  return
+	return
 end
 
 -- -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -26,24 +26,25 @@ end
 --     },
 -- })
 
-null_ls.setup {
-  cmd = { "nvim" },
-  debounce = 250,
-  debug = false,
-  default_timeout = 5000,
-  diagnostic_config = nil,
-  diagnostics_format = "#{m}",
-  fallback_severity = vim.diagnostic.severity.ERROR,
-  log_level = "warn",
-  notify_format = "[null-ls] %s",
-  on_attach = nil,
-  on_init = nil,
-  on_exit = nil,
-  root_dir = require("null-ls.utils").root_pattern(".null-ls-root", "Makefile", ".git"),
-  sources = {
-    require("null-ls").builtins.formatting.black,
-    require("null-ls").builtins.diagnostics.flake8,
-    -- require("null-ls").builtins.completion.stylua,
-  },
-  update_in_insert = false,
-}
+null_ls.setup({
+	cmd = { "nvim" },
+	debounce = 250,
+	debug = false,
+	default_timeout = 5000,
+	diagnostic_config = nil,
+	diagnostics_format = "#{m}",
+	fallback_severity = vim.diagnostic.severity.ERROR,
+	log_level = "warn",
+	notify_format = "[null-ls] %s",
+	on_attach = nil,
+	on_init = nil,
+	on_exit = nil,
+	root_dir = require("null-ls.utils").root_pattern(".null-ls-root", "Makefile", ".git"),
+	sources = {
+		require("null-ls").builtins.formatting.black,
+		require("null-ls").builtins.diagnostics.flake8,
+		require("null-ls").builtins.formatting.stylua,
+		require("null-ls").builtins.diagnostics.selene,
+	},
+	update_in_insert = false,
+})
