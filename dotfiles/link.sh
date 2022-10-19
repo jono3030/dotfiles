ae6c4714b22101b12c43801af616cef03ae73a61
jono3030 <j.osmann@mail.utoronto.ca>
2022-10-19 12:47:35 -0400

    [wip] figure out symlinks still.

--- dotfiles/link.sh
+++ dotfiles/link.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-USER=$(whoami)
-echo $USER
+SCR_USER=$(whoami)
 
-#mv /home/jonas/dotfiles/dotfiles/* /home/jonas/dotfiles/.
-#rm -r /home/jonas/dotfiles/dotfiles
-ln -s /home/jonas/dotfiles/dotfiles/.task /home/jonas/.task
-ln -s /home/jonas/dotfiles/dotfiles/.taskrc /home/jonas/.taskrc
-ln -s /home/jonas/dotfiles/dotfiles/.timewarrior /home/jonas/.timewarrior
-ln -s /home/jonas/dotfiles/dotfiles/.vim /home/jonas/.vim
-ln -s /home/jonas/dotfiles/dotfiles/.viminfo /home/jonas/.viminfo
-ln -s /home/jonas/dotfiles/dotfiles/.vimrc /home/jonas/.vimrc
-ln -s /home/jonas/dotfiles/dotfiles/.vimwiki /home/jonas/.vimwiki
-sed -i "s/\(data.location=\/\).*\(\/.task\)/\1\home\/jonas\2/g" /home/jonas/.taskrc
-#sed "s/\(data.location=\/\).*\(\/.task\)/\1\home\/$(whoami)\2/g" /home/jonas/.taskrc
+#mv /home/$SCR_USER/dotfiles/dotfiles/* /home/$SCR_USER/dotfiles/.
+#rm -r /home/$SCR_USER/dotfiles/dotfiles
+ln -s /home/$SCR_USER/dotfiles/dotfiles/.task /home/$SCR_USER/.task
+ln -s /home/$SCR_USER/dotfiles/dotfiles/.taskrc /home/$SCR_USER/.taskrc
+ln -s /home/$SCR_USER/dotfiles/dotfiles/.timewarrior /home/$SCR_USER/.timewarrior
+ln -s /home/$SCR_USER/dotfiles/dotfiles/.vim /home/$SCR_USER/.vim
+ln -s /home/$SCR_USER/dotfiles/dotfiles/.viminfo /home/$SCR_USER/.viminfo
+ln -s /home/$SCR_USER/dotfiles/dotfiles/.vimrc /home/$SCR_USER/.vimrc
+ln -s /home/$SCR_USER/dotfiles/dotfiles/.vimwiki /home/$SCR_USER/.vimwiki
+sed -i "s/\(data.location=\/\).*\(\/.task\)/\1\home\/$SCR_USER\2/g" /home/$SCR_USER/.taskrc
+#sed "s/\(data.location=\/\).*\(\/.task\)/\1\home\/$(whoami)\2/g" /home/$SCR_USER/.taskrc
