3df292851961f57d712ea70b20626ec22ad11336
jono3030 <j.osmann@mail.utoronto.ca>
2022-10-19 12:26:58 -0400

    [wip] figure out symlinks still.

--- dotfiles/link.sh
+++ dotfiles/link.sh
@@ -3,14 +3,14 @@
 USER=$(whoami)
 echo $USER
 
-mv /home/jonas/dotfiles/dotfiles/* /home/jonas/dotfiles/.
-rm -r /home/jonas/dotfiles/dotfiles
-ln -s /home/jonas/dotfiles/.task /home/jonas/.task
-ln -s /home/jonas/dotfiles/.taskrc /home/jonas/.taskrc
-ln -s /home/jonas/dotfiles/.timewarrior /home/jonas/.timewarrior
-ln -s /home/jonas/dotfiles/.vim /home/jonas/.vim
-ln -s /home/jonas/dotfiles/.viminfo /home/jonas/.viminfo
-ln -s /home/jonas/dotfiles/.vimrc /home/jonas/.vimrc
-ln -s /home/jonas/dotfiles/.vimwiki /home/jonas/.vimwiki
+#mv /home/jonas/dotfiles/dotfiles/* /home/jonas/dotfiles/.
+#rm -r /home/jonas/dotfiles/dotfiles
+ln -s /home/jonas/dotfiles/dotfiles/.task /home/jonas/.task
+ln -s /home/jonas/dotfiles/dotfiles/.taskrc /home/jonas/.taskrc
+ln -s /home/jonas/dotfiles/dotfiles/.timewarrior /home/jonas/.timewarrior
+ln -s /home/jonas/dotfiles/dotfiles/.vim /home/jonas/.vim
+ln -s /home/jonas/dotfiles/dotfiles/.viminfo /home/jonas/.viminfo
+ln -s /home/jonas/dotfiles/dotfiles/.vimrc /home/jonas/.vimrc
+ln -s /home/jonas/dotfiles/dotfiles/.vimwiki /home/jonas/.vimwiki
 sed "s/\(data.location=\/\).*\(\/.task\)/\1\home\/jonas\2/g" /home/jonas/.taskrc
 #sed "s/\(data.location=\/\).*\(\/.task\)/\1\home\/$(whoami)\2/g" /home/jonas/.taskrc
