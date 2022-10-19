1e9d7be4919b80f3ec6ddaef1877d3aa0f159e47
jono3030 <j.osmann@mail.utoronto.ca>
2022-10-19 10:23:17 -0400

    [wip] add Bash script to do symlinking

---
+++ dotfiles/link.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln -s /home/jonas/dotfiles/dotfiles/.task /home/jonas/.task
+ln -s /home/jonas/dotfiles/dotfiles/.taskrc /home/jonas/.taskrc
+ln -s /home/jonas/dotfiles/dotfiles/.timewarrior /home/jonas/.timewarrior
+ln -s /home/jonas/dotfiles/dotfiles/.vim /home/jonas/.vim
+ln -s /home/jonas/dotfiles/dotfiles/.viminfo /home/jonas/.viminfo
+ln -s /home/jonas/dotfiles/dotfiles/.vimrc /home/jonas/.vimrc
+ln -s /home/jonas/dotfiles/dotfiles/.vimwiki /home/jonas/.vimwiki
