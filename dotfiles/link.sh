34a4741ba8aed67340fd56dc38d2b71325c76208
jono3030 <j.osmann@mail.utoronto.ca>
2022-12-01 23:43:24 -0500

    [feat] add calcurse and taskwarrior-tui symlink

--- dotfiles/link.sh
+++ dotfiles/link.sh
@@ -3,6 +3,10 @@
 # Get system user
 SCR_USER=$(whoami)
 
+# Create directories required for symlinks
+mkdir -p /home/$SCR_USER/.config
+mkdir -p /home/$SCR_USER/.local/share
+
 # Make symlinks from sync folder to container
 #mv /home/$SCR_USER/dotfiles/dotfiles/* /home/$SCR_USER/dotfiles/.
 #rm -r /home/$SCR_USER/dotfiles/dotfiles
@@ -14,9 +18,8 @@ ln -s /home/$SCR_USER/dotfiles_sync/.vim /home/$SCR_USER/.vim
 ln -s /home/$SCR_USER/dotfiles_sync/.viminfo /home/$SCR_USER/.viminfo
 ln -s /home/$SCR_USER/dotfiles_sync/.vimrc /home/$SCR_USER/.vimrc
 ln -s /home/$SCR_USER/dotfiles_sync/vimwiki /home/$SCR_USER/vimwiki
-mkdir -p /home/$SCR_USER/.config
-mkdir -p /home/$SCR_USER/.local/share
 ln -s /home/$SCR_USER/dotfiles_sync/.config/calcurse /home/$SCR_USER/.config/calcurse
 ln -s /home/$SCR_USER/dotfiles_sync/.local/share/calcurse /home/$SCR_USER/.local/share/calcurse
+ln -s /home/$SCR_USER/dotfiles_sync/.local/share/taskwarrior-tui /home/$SCR_USER/.local/share/taskwarrior-tui
 #sed -i "s/\(data.location=\/\).*\(\/.task\)/\1\home\/$SCR_USER\2/g" /home/$SCR_USER/.taskrc
 #sed "s/\(data.location=\/\).*\(\/.task\)/\1\home\/$(whoami)\2/g" /home/$SCR_USER/.taskrc
