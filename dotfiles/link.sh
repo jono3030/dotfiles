cc349fba058c6a3eeb70b09c824181e209007cc4
jono3030 <j.osmann@mail.utoronto.ca>
2022-12-01 22:46:22 -0500

    [fix] fix taskrc symlink issue still

--- dotfiles/link.sh
+++ dotfiles/link.sh
@@ -12,6 +12,5 @@ ln -s /home/$SCR_USER/dotfiles_sync/.vim /home/$SCR_USER/.vim
 ln -s /home/$SCR_USER/dotfiles_sync/.viminfo /home/$SCR_USER/.viminfo
 ln -s /home/$SCR_USER/dotfiles_sync/.vimrc /home/$SCR_USER/.vimrc
 ln -s /home/$SCR_USER/dotfiles_sync/vimwiki /home/$SCR_USER/vimwiki
-ln -s /home/$SCR_USER/dotfiles_sync/.taskrc /home/$SCR_USER/.taskrc
 #sed -i "s/\(data.location=\/\).*\(\/.task\)/\1\home\/$SCR_USER\2/g" /home/$SCR_USER/.taskrc
 #sed "s/\(data.location=\/\).*\(\/.task\)/\1\home\/$(whoami)\2/g" /home/$SCR_USER/.taskrc
