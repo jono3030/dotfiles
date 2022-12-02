1e2c7251922359288404cd531c266c278297df29
jono3030 <j.osmann@mail.utoronto.ca>
2022-12-01 23:12:26 -0500

    [fix] symlinks added and fixed
    Fixed .taskrc symlink and added symlinks for calcurse files.

--- dotfiles/link.sh
+++ dotfiles/link.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Get system user
 SCR_USER=$(whoami)
 
+# Make symlinks from sync folder to container
 #mv /home/$SCR_USER/dotfiles/dotfiles/* /home/$SCR_USER/dotfiles/.
 #rm -r /home/$SCR_USER/dotfiles/dotfiles
 #rm /home/$SCR_USER/.taskrc
@@ -12,5 +14,7 @@ ln -s /home/$SCR_USER/dotfiles_sync/.vim /home/$SCR_USER/.vim
 ln -s /home/$SCR_USER/dotfiles_sync/.viminfo /home/$SCR_USER/.viminfo
 ln -s /home/$SCR_USER/dotfiles_sync/.vimrc /home/$SCR_USER/.vimrc
 ln -s /home/$SCR_USER/dotfiles_sync/vimwiki /home/$SCR_USER/vimwiki
+ln -s /home/$SCR_USER/dotfiles_sync/.config/calcurse /home/$SCR_USER/.config/calcurse
+ln -s /home/$SCR_USER/dotfiles_sync/.local/share/calcurse /home/$SCR_USER/.local/share/calcurse
 #sed -i "s/\(data.location=\/\).*\(\/.task\)/\1\home\/$SCR_USER\2/g" /home/$SCR_USER/.taskrc
 #sed "s/\(data.location=\/\).*\(\/.task\)/\1\home\/$(whoami)\2/g" /home/$SCR_USER/.taskrc
