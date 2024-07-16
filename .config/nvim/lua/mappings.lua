4e127faddf357d1c1ece0b31f8911a55bcb10753
Jonas Osmann <j.osmann@mail.utoronto.ca>
2024-07-16 16:21:22 -0400

    [wip] first implementation of debugger
    Implemented cpp debugger with nvim-dap and nvim-dap-ui. It's working but
    requires a lot more testing before it becomes useable.

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -8,3 +8,19 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+map("n", "<leader>db", "<cmd>DapToggleBreakpoint<CR>", { desc = "Add breakpoint at line" })
+map("n", "<leader>dr", "<cmd>DapContinue<CR>", { desc = "Start or continue the debugger" })
+
+-- local dap_mappings = {
+--   n = {
+--     ["<leader>db"] = {
+--       "<cmd>DapToggleBreakpoint<CR>",
+--       "Add breakpoint at line",
+--     },
+--     ["<leader>dr"] = {
+--       "<cmd>DapContinue<CR>",
+--       "Start or continue the debugger",
+--     },
+--   },
+-- }
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
   {
     "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre", -- uncomment for format on save
     config = function()
       require "configs.conform"
     end,
@@ -17,30 +17,77 @@ return {
   },
   --
   {
-  	"williamboman/mason.nvim",
-  	opts = {
-  		ensure_installed = {
-  			"lua-language-server", "stylua",
-        "clangd", "clang-format"
-  		},
-  	},
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "clangd",
+        "clang-format",
+        "codelldb",
+      },
+    },
   },
   --
   {
-  	"nvim-treesitter/nvim-treesitter",
-  	opts = {
-  		ensure_installed = {
-  			"vim", "lua", "vimdoc",
-       "cpp"
-  		},
-  	},
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-nio",
+    },
+    config = function()
+      local dap = require "dap"
+      local dapui = require "dapui"
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+  },
+  --
+  {
+    "nvim-neotest/nvim-nio",
+  },
+  --
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    opts = {
+      handlers = {},
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+  },
+  --
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "cpp",
+      },
+    },
   },
   --
   {
     "jose-elias-alvarez/null-ls.nvim",
     event = "VeryLazy",
     config = function()
-      require("configs.null-ls")
+      require "configs.null-ls"
     end,
-  }
+  },
 }
