25140be0bfa4c0f16e7e04ddc4ee76eee56e5782
Jonas Osmann <j.osmann@mail.utoronto.ca>
2024-07-16 15:30:47 -0400

    [wip] add custom options overrides

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -4,3 +4,9 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+local opt = vim.opt
+
+opt.colorcolumn = "80"
+opt.relativenumber = true
+opt.scrolloff = 8
