29a785f3088ac383ac1bdff0fd58169edd66fcd9
Jonas Osmann <j.osmann@mail.utoronto.ca>
2024-08-22 11:32:25 -0400

    [wip] add harpoon 2 plugin

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -93,6 +93,16 @@ return {
   },
   --
   {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    event = "VeryLazy",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require "configs.harpoon"
+    end,
+  },
+  --
+  {
     "m4xshen/hardtime.nvim",
     event = "VeryLazy",
     dependencies = {
